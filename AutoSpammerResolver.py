039da33271915c8ac12c79d9d708a761e2be870c
Akito Miura <phenomer@g.hachune.net>
2024-02-18 03:04:24 +0900

    init AutoSpammerResolver

---
+++ AutoSpammerResolver.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python3
+
+import requests
+from requests import Response
+import json
+import sys
+
+config = json.loads(open("config.json", "r").read())
+
+
+class AutoSpammerResolver:
+    class ProtocolError(Exception):
+        pass
+
+    def __init__(self, don_url: str, access_token: str):
+        self.don_url = don_url
+        self.access_token = access_token
+
+    def response_validator(self, res: Response) -> bool:
+        if res.status_code == 200:
+            return True
+        else:
+            raise AutoSpammerResolver.ProtocolError(
+                "{} {} - {}".format(res.status_code, res.reason, res.request.url)
+            )
+
+    def authentication_header(self):
+        return {"Authorization": f"Bearer {self.access_token}"}
+
+    # レポート一覧取得: https://docs.joinmastodon.org/methods/admin/reports/#get
+    # デフォルトでは未解決のもののみ返す。
+    # resolved=Trueで解決済みのものを返す。
+    def get_reports(self, limit: int = 5, resolved: bool = False) -> dict:
+        uri = f"{self.don_url}/api/v1/admin/reports"
+        params = {"limit": str(limit)}
+        if resolved:
+            params["resolved"] = "1"
+        res = requests.get(
+            uri,
+            headers=self.authentication_header(),
+            params=params,
+        )
+        if self.response_validator(res):
+            return res.json()
+
+    # サスペンド: https://docs.joinmastodon.org/methods/admin/accounts/#action
+    def suspend(self, target_id: str, report_id: str) -> dict:
+        uri = f"{self.don_url}/api/v1/admin/accounts/{target_id}/action"
+        res = requests.post(
+            uri,
+            headers=self.authentication_header(),
+            data={"type": "suspend", "report_id": report_id},
+        )
+        if self.response_validator(res):
+            return res.json()
+
+    # レポート解決: https://docs.joinmastodon.org/methods/admin/reports/#resolve
+    # サスペンド時にreport_idを付与しておけば自動的に解決されるので、
+    # 通常は個別に処理する必要はない。
+    def resolve_report(self, report_id: str) -> dict:
+        uri = f"{self.don_url}/api/v1/admin/reports/{report_id}/resolve"
+        res = requests.post(
+            uri, headers=self.authentication_header(), data={"report_id": report_id}
+        )
+        if self.response_validator(res):
+            return res.json()
+
+
+auto_resolver = AutoSpammerResolver(
+    don_url=config["DonURL"], access_token=config["AccessToken"]
+)
+
+
+# 始末するターゲットであればTrue、始末しなくてもよければFalseを返す。
+# 状況に応じて書き換えていく。
+def target_check(report: dict) -> bool:
+    # spamカテゴリで報告されたもの以外は自動処理しない
+    if report["category"] != "spam":
+        return False
+
+    # ターゲットユーザ名の長さが10文字以外の場合は自動処理しない
+    if len(report["target_account"]["username"]) != 10:
+        return False
+
+    # 自身のサーバ以外から(外部サーバから)のレポートである場合は自動処理しない
+    if report["account"]["domain"] is not None:
+        return False
+    return True
+
+
+resolved = False
+if len(sys.argv) == 2 and sys.argv[1] == "resolved":
+    resolved = True
+
+# 直近10件の未解決レポートを取得する。
+for report in auto_resolver.get_reports(limit=10, resolved=resolved):
+    print("-" * 10)
+    print(f"ReportID: {report['id']}")
+    print(f"Category: {report['category']}")
+    print(f"ActionTaken: {report['action_taken']}")
+    print(f"TakenAt: {report['action_taken_at']}")
+    print(f"Comment: {report['comment']}")
+    print(f"Forwarded: {report['forwarded']}")
+    print(f"CreatedAt: {report['created_at']}")
+    print(f"UpdatedAt: {report['updated_at']}")
+    print(f"Reporter: {report['account']['username']}@{report['account']['domain']}")
+    print(
+        f"Target: {report['target_account']['username']}@{report['target_account']['domain']} ({report['target_account']['id']})"
+    )
+    print(f"Suspended: {report['target_account']['suspended']}\n")
+    if not target_check(report):
+        print("Skipped.")
+        continue
+    # 対象アカウントをサスペンドする。
+    if len(sys.argv) == 2 and sys.argv[1] == "execute":
+        auto_resolver.suspend(
+            target_id=report["target_account"]["id"], report_id=report["id"]
+        )
+        # suspend処理の際にreport_idを付けとくと、別途resolveしなくてもいいらしい
+        # auto_resolver.resolve_report(report_id=report['id'])
+        print("Executed.")
---
+++ README.md
@@ -0,0 +1,101 @@
+# AutoSpammerResolver
+
+## これはなに?
+Mastodonでユーザーにより報告されたspamを、問答無用で始末(サスペンド)するスクリプトです。
+利用には下記の権限が必要です。
+
+- admin:read:reports
+- admin:write:accounts
+- admin:write:reports
+
+
+## つかいかた
+「開発」 -> 新規アプリから、アプリを作成してください。
+
+- アプリの名前: てきとうに名前を付ける
+- アプリのウェブサイト: 空欄
+- リダイレクトURI: urn:ietf:wg:oauth:2.0:oob
+- アクセス権
+  - admin:read:reports
+  - admin:write:accounts
+  - admin:write:reports
+
+作成できたら、生成された**アクセストークン**を、
+config.jsonの**AccessToken**に記述してください。
+また、DonURLを自身が管理しているMastodonサーバのURLにしてください。
+
+```json
+{
+  "DonURL": "https://social.mikutter.hachune.net",
+  "AccessToken": "生成されたアクセストークン"
+}
+```
+
+### 未解決の報告を確認する
+実行すると、下記のように対象者の情報が表示されます。
+デフォルトでは最新10件分を処理します。
+
+```sh
+% python3 AutoSpammerResolver.py
+ReportID: 1341
+Category: spam
+ActionTaken: True
+TakenAt: 2024-02-17T16:47:32.955Z
+Comment:
+Forwarded: True
+CreatedAt: 2024-02-17T16:31:08.772Z
+UpdatedAt: 2024-02-17T16:47:32.958Z
+Reporter: reporter_name@None
+Target: target_user@target_domain (111947772733136565)
+Suspended: False
+...
+```
+
+### 始末する
+実際に始末する際は、`execute`オプションを付けて実行してください。
+```sh
+% python3 AutoSpammerResolver.py execute
+ReportID: 1341
+Category: spam
+ActionTaken: True
+TakenAt: 2024-02-17T16:47:32.955Z
+Comment:
+Forwarded: True
+CreatedAt: 2024-02-17T16:31:08.772Z
+UpdatedAt: 2024-02-17T16:47:32.958Z
+Reporter: reporter_name@None
+Target: target_user@target_domain (111947772733136565)
+Suspended: False
+
+Executed.
+```
+始末すると、対象となったアカウントはサスペンドされ、
+レポートにも解決フラグが設定されます。
+
+
+### 解決済みのレポートを確認する
+
+既に解決済みのレポートを確認したい時は、`resolved`オプションを付けて実行してください。
+
+```sh
+% python3 AutoSpammerResolver.py resolved
+ReportID: 1341
+Category: spam
+ActionTaken: True
+TakenAt: 2024-02-17T16:47:32.955Z
+Comment:
+Forwarded: True
+CreatedAt: 2024-02-17T16:31:08.772Z
+UpdatedAt: 2024-02-17T16:47:32.958Z
+Reporter: reporter_name@None
+Target: target_user@target_domain (111947772733136565)
+Suspended: True
+```
+
+## カスタマイズ
+`target_check`メソッドを変更すると、対象を絞り込んだりできます。
+デフォルトでは、下記の条件を全て満たしたもののみ自動で処理します。
+
+- 報告者は自インスタンスのもののみ(スパム報告スパム対策)
+- 対象ユーザー名は10文字のもの
+- spamとして報告されたもの
